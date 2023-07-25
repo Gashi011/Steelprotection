3b96b12a715b05554b986a9a079d1c4cd5da499b
Gashi011 <igorgasi@gmail.com>
2023-07-26 00:59:21 +0200

    Uradjen Hamburger menu

--- steelprot-app/components/HamburgerMenu.tsx
+++ steelprot-app/components/HamburgerMenu.tsx
@@ -1,45 +1,44 @@
-import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IoMenuSharp } from "react-icons/io5";
+import { FaTimes } from "react-icons/fa";
+import Link from "next/link";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Set the isClient state to true when the component mounts on the client-side
+  }, []);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  if (!isClient) {
+    // If the component is not yet mounted on the client-side (during server-side rendering), return null
+    return null;
+  }
+
   return (
     <div className="lg:hidden">
-      <button
-        onClick={handleToggle}
-        className="text-black  focus:outline-none"
-      >
+      {/* Use a div instead of a button for the container */}
+      <div onClick={handleToggle} className="text-black focus:outline-none">
         {isOpen ? (
           // Custom icon for the open state
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0  24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <FaTimes className="w-6 h-6 text-neutral-700" />
         ) : (
-          // Custom icon for the closed state (using your IoMenuSharp icon)
-          <IoMenuSharp className="w-10 h-10 text-neutral-600" />
+          // Custom icon for the closed state (using IoMenuSharp icon)
+          <IoMenuSharp className="w-6 h-6" />
         )}
-      </button>
+      </div>
       {isOpen && (
         <div className="mt-2 absolute right-10 flex flex-col gap-10 text-xl">
-          <a href="#" className="text-neutral-500 py-2"><Link href="/about"> About</Link></a>
-          <a href="#" className="text-neutral-500 py-2">Home</a>
-          <a href="#" className="text-neutral-500 py-2">Contact</a>
+          <Link href="/about">
+            About
+          </Link>
+          <div style={{ outline: "none" }}>Home</div>
+          <div style={{ outline: "none" }}>Contact</div>
         </div>
       )}
     </div>
--- steelprot-app/components/Search.tsx
+++ steelprot-app/components/Search.tsx
@@ -5,10 +5,10 @@ import HamburgerMenu from "./HamburgerMenu";
 const Search = () => {
   return (
 
-    <Link href="/about">
+    
       <button className="md:hidden w-6 h-6"><HamburgerMenu /></button>
       
-    </Link>
+    
   );
 };
 
